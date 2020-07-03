9d93223c9d7f78c071a7c19aa8bf2166edeb00c4
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-03 16:12:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by KPS on 7/3/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+         final double width = 5.5 ;
+         final double height = 8.5;
+         double perimiter = 2*(width+height);
+         double area = (width*height);
+        System.out.println("perimiter : "+perimiter);
+        System.out.println("area : "+area);
+    }
+}
